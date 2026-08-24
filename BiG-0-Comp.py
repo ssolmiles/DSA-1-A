655ab0667c2587598efe7672ae7aa8f7611c617b
Geres Maria Gutierrez <xaiean2@gmail.com>
2026-08-24 23:41:57 +0800

    O-N-Linear.py

--- BiG-0-Comp.py
+++ BiG-0-Comp.py
@@ -8,3 +8,14 @@ numbers = [10, 20, 30, 40, 50]
 print(get_first(numbers))  # 10
 # O(1) constant
 
+
+def find_max(arr):
+    max_val = arr[0]
+    for x in arr:
+        if x > max_val:
+            max_val = x
+    return max_val
+
+# O(n) linear
+numbers = [3, 7, 2, 9, 4]
+print(find_max(numbers))  
