a4ac552f6ac9e8beae5ba62d5b4aad25a895dd69
Geres Maria Gutierrez <xaiean2@gmail.com>
2026-09-21 02:33:49 +0800

    Grokkings 2

--- DSA-3.py
+++ DSA-3.py
@@ -19,3 +19,29 @@ my_list = [1, 3, 5, 7, 9]
 
 print(binary_search(my_list, 3))  
 print(binary_search(my_list, -1))
+
+
+
+def find_smallest(arr):
+    smallest = arr[0]
+    smallest_index = 0
+
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+
+    return smallest_index
+
+
+def selection_sort(arr):
+    new_arr = []
+
+    for i in range(len(arr)):
+        smallest = find_smallest(arr)
+        new_arr.append(arr.pop(smallest))
+
+    return new_arr
+
+
+print(selection_sort([5, 3, 6, 2, 10]))
